c3d7d8044653f4c6a82d37f3a591a417b86b003f
Jagostini <agostini.j.85@gmail.com>
2018-04-12 17:41:00 +0200

    home

--- workshop-web/src/app/home/home.component.ts
+++ workshop-web/src/app/home/home.component.ts
@@ -16,26 +16,30 @@ export class HomeComponent implements OnInit {
   constructor(private route: Router) {}
 
 
-  etudiant1: Etudiant = { id: 5, idGroupe: 5, prenom: "toto", nom: "toto" , classe: "EPSI B3", email: "toto@gmail.com", motDePasse: "toto" };
-  etudiant2: Etudiant = { id: 5, idGroupe: 6, prenom: "tata", nom: "tata" , classe: "EPSI B3", email: "tata@gmail.com", motDePasse: "tata" };
+  // tslint:disable-next-line:max-line-length
+  etudiant1: Etudiant = { id: 5, idGroupe: 5, prenom: 'toto', nom: 'toto' , classe: 'EPSI B3', email: 'toto@gmail.com', motDePasse: 'toto' };
+  // tslint:disable-next-line:max-line-length
+  etudiant2: Etudiant = { id: 5, idGroupe: 6, prenom: 'tata', nom: 'tata' , classe: 'EPSI B3', email: 'tata@gmail.com', motDePasse: 'tata' };
 
-  administration1: Administration = { id: 5, nom: "admin", prenom: "admin", motDePasse: "admin" };
-  administration2: Administration = { id: 6, nom: "admin2", prenom: "admin2", motDePasse: "admin2" };
+  administration1: Administration = { id: 5, nom: 'admin', prenom: 'admin', motDePasse: 'admin' };
+  administration2: Administration = { id: 6, nom: 'admin2', prenom: 'admin2', motDePasse: 'admin2' };
 
-  intervenant1: intervenant = { id: 10, idProjet: 1, nom: "Musk", prenom: "Elon", competence: "java, javacript", disponibilite: "plein de chose", tempsRealise: 10, motDePasse:"Elon"};
-  intervenant2: intervenant = { id: 11, idProjet: 2, nom: "Tourman", prenom: "Benjamin", competence: "JVM", disponibilite: "plein de truc", tempsRealise: 2, motDePasse:"benjamin"};
+  // tslint:disable-next-line:max-line-length
+  intervenant1: intervenant = { id: 10, idProjet: 1, nom: 'Musk', prenom: 'Elon', competence: 'java, javacript', disponibilite: 'plein de chose', tempsRealise: 10, motDePasse: 'Elon'};
+  // tslint:disable-next-line:max-line-length
+  intervenant2: intervenant = { id: 11, idProjet: 2, nom: 'Tourman', prenom: 'Benjamin', competence: 'JVM', disponibilite: 'plein de truc', tempsRealise: 2, motDePasse: 'benjamin'};
 
   login: string;
   motDePasse: string;
 
   role = 1;
-  etat=true;
+  etat = true;
   routAdmin = false;
 
   ngOnInit() {
   }
 
-  changeRole(r:number) {
+  changeRole(r: number) {
     this.role = r;
   }
 
@@ -48,41 +52,43 @@ export class HomeComponent implements OnInit {
 
     console.log(this.administration1.motDePasse.localeCompare(this.motDePasse));
     console.log(this.administration1.nom.localeCompare(this.login));
-    this.administration1.motDePasse.localeCompare(this.motDePasse)
-    this.administration1.nom.localeCompare(this.login)
+    this.administration1.motDePasse.localeCompare(this.motDePasse);
+    this.administration1.nom.localeCompare(this.login);
 
-    if(this.role == 1) {
-      if(this.administration1.motDePasse.localeCompare(this.motDePasse) === 0 && this.administration1.nom.localeCompare(this.login) === 0) {
-        console.log("bravo");
-        this.etat=true;
+    if (this.role === 1) {
+      // tslint:disable-next-line:max-line-length
+      if (this.administration1.motDePasse.localeCompare(this.motDePasse) === 0 && this.administration1.nom.localeCompare(this.login) === 0) {
+        console.log('bravo');
+        this.etat = true;
         this.routAdmin = true;
         this.route.navigate(['admin']);
       } else {
-        this.etat=false;
-        console.log("connexion refusé");
+        this.etat = false;
+        console.log('connexion refusé');
       }
-      
+
     }
-    if(this.role == 3) {
-      if(this.etudiant1.motDePasse.localeCompare(this.motDePasse) === 0 && this.etudiant1.nom.localeCompare(this.login) === 0) {
-        console.log("gotoEtudiant");
-        this.etat=true;
+    if (this.role === 3) {
+      if (this.etudiant1.motDePasse.localeCompare(this.motDePasse) === 0 && this.etudiant1.nom.localeCompare(this.login) === 0) {
+        console.log('gotoEtudiant');
+        this.etat = true;
         this.route.navigate(['eleve']);
       } else {
-        this.etat=false;
-        console.log("connexion refusé");
+        this.etat = false;
+        console.log('connexion refusé');
       }
-      
+
     }
-    if(this.role == 2) {
-      if(this.intervenant1.motDePasse.localeCompare(this.motDePasse) === 0 && this.intervenant1.nom. localeCompare(this.motDePasse) === 0) {
-        console.log("gotoIntervenant");
-        this.etat=true;
+    if (this.role === 2) {
+      // tslint:disable-next-line:max-line-length
+      if (this.intervenant1.motDePasse.localeCompare(this.motDePasse) === 0 && this.intervenant1.nom. localeCompare(this.motDePasse) === 0) {
+        console.log('gotoIntervenant');
+        this.etat = true;
       } else {
-        this.etat=false;
-        console.log("connexion refusé");
+        this.etat = false;
+        console.log('connexion refusé');
       }
-      
+
     }
   }
 
